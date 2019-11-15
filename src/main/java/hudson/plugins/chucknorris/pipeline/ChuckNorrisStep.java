e0bc12df69e2cf8b049afa84f321f77b991748a0
Christian.Rohr <Ch.Rohr@gmx.de>
2019-11-15 23:28:33 +0100

    Changed class as subclass

--- src/main/java/hudson/plugins/chucknorris/pipeline/ChuckNorrisStep.java
+++ src/main/java/hudson/plugins/chucknorris/pipeline/ChuckNorrisStep.java
@@ -1,7 +1,12 @@
 package hudson.plugins.chucknorris.pipeline;
 
+import hudson.Extension;
 import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Set;
 
 public class ChuckNorrisStep extends Step {
 
@@ -14,9 +19,25 @@ public class ChuckNorrisStep extends Step {
 		return stepContext.get(ChuckNorrisStepExecution.class);
 	}
 
-	@Override
-	public StepDescriptor getDescriptor() {
-		return new ChuckNorrisStepDescriptor();
+	@Extension
+	public static class ChuckNorrisStepDescriptor extends StepDescriptor {
+
+		@Override
+		public Set<? extends Class<?>> getRequiredContext() {
+			return Collections.singleton(ChuckNorrisStepExecution.class);
+		}
+
+		@Override
+		public String getFunctionName() {
+			return "chuckNorris";
+		}
+
+		@Nonnull
+		@Override
+		public String getDisplayName() {
+			return "Submit to Chuck Norris' will";
+		}
+
 	}
 
 }
