24f036037db9ec40f593df95539c50548a59a786
Basil Crow <me@basilcrow.com>
2023-07-27 09:26:13 -0700

    Refresh plugin for July 2023 (#208)

--- src/test/java/hudson/plugins/chucknorris/pipeline/ChuckNorrisStepTest.java
+++ src/test/java/hudson/plugins/chucknorris/pipeline/ChuckNorrisStepTest.java
@@ -9,8 +9,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runners.model.Statement;
-import org.jvnet.hudson.test.RestartableJenkinsRule;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import hudson.model.Result;
 import hudson.plugins.chucknorris.RoundhouseAction;
@@ -18,91 +17,73 @@ import hudson.plugins.chucknorris.Style;
 
 public class ChuckNorrisStepTest {
 	@Rule
-	public RestartableJenkinsRule story = new RestartableJenkinsRule();
+	public JenkinsRule j = new JenkinsRule();
 
     @Test
     public void badAssChuckNorris() throws Exception {
-		story.addStep(new Statement() {
-			@Override
-			public void evaluate() throws Throwable {
-				WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
+				WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 				p.setDefinition(new CpsFlowDefinition(
 						"chuckNorris()\n" +
 						"semaphore 'wait'\n"
-				));
+				, true));
 				WorkflowRun b1 = p.scheduleBuild2(0).waitForStart();
 				SemaphoreStep.waitForStart("wait/1", b1);
 				SemaphoreStep.failure("wait/1", new Exception());
 				
-				story.j.waitForCompletion(b1);
-				story.j.assertBuildStatus(Result.FAILURE, b1);
+				j.waitForCompletion(b1);
+				j.assertBuildStatus(Result.FAILURE, b1);
 				
 				RoundhouseAction action = b1.getAction(RoundhouseAction.class);
 				assertEquals(Style.BAD_ASS, action.getStyle());
 				assertNotNull(action.getFact());
-			}
-		});
     }
 
     @Test
     public void alertChuckNorris() throws Exception {
-		story.addStep(new Statement() {
-			@Override
-			public void evaluate() throws Throwable {
-				WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
+				WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 				p.setDefinition(new CpsFlowDefinition(
 						"chuckNorris()\n" +
 						"semaphore 'wait'\n"
-				));
+				, true));
 				WorkflowRun b1 = p.scheduleBuild2(0).waitForStart();
 				SemaphoreStep.waitForStart("wait/1", b1);
 				b1.setResult(Result.UNSTABLE);
 				SemaphoreStep.success("wait/1", null);
 				
-				story.j.waitForCompletion(b1);
-				story.j.assertBuildStatus(Result.UNSTABLE, b1);
+				j.waitForCompletion(b1);
+				j.assertBuildStatus(Result.UNSTABLE, b1);
 				
 				RoundhouseAction action = b1.getAction(RoundhouseAction.class);
 				assertEquals(Style.ALERT, action.getStyle());
 				assertNotNull(action.getFact());
-			}
-		});
     }
 
     @Test
     public void thumbsUpChuckNorris() throws Exception {
-		story.addStep(new Statement() {
-			@Override
-			public void evaluate() throws Throwable {
-				WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
+				WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 				p.setDefinition(new CpsFlowDefinition(
 						"chuckNorris()\n" +
 						"semaphore 'wait'\n"
-				));
+				, true));
 				WorkflowRun b1 = p.scheduleBuild2(0).waitForStart();
 				SemaphoreStep.waitForStart("wait/1", b1);
 				SemaphoreStep.success("wait/1", null);
 				
-				story.j.waitForCompletion(b1);
-				story.j.assertBuildStatus(Result.SUCCESS, b1);
+				j.waitForCompletion(b1);
+				j.assertBuildStatus(Result.SUCCESS, b1);
 				
 				RoundhouseAction action = b1.getAction(RoundhouseAction.class);
 				assertEquals(Style.THUMB_UP, action.getStyle());
 				assertNotNull(action.getFact());
-			}
-		});
     }
 
     @Test
     public void projectPageChuckNorris() throws Exception {
-		story.addStep(new Statement() {
-			@Override
-			public void evaluate() throws Throwable {
-				WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
+				WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 				p.setDefinition(new CpsFlowDefinition(
 						"chuckNorris()\n" +
 						"semaphore 'wait'\n"
-				));
+				, true));
 				WorkflowRun b1 = p.scheduleBuild2(0).waitForStart();
 				SemaphoreStep.waitForStart("wait/1", b1);
 				
@@ -112,11 +93,11 @@ public class ChuckNorrisStepTest {
 				SemaphoreStep.success("wait/1", null);
 				SemaphoreStep.failure("wait/2", new Exception());
 				
-				story.j.waitForCompletion(b1);
-				story.j.assertBuildStatus(Result.SUCCESS, b1);
+				j.waitForCompletion(b1);
+				j.assertBuildStatus(Result.SUCCESS, b1);
 				
-				story.j.waitForCompletion(b1);
-				story.j.assertBuildStatus(Result.FAILURE, b2);
+				j.waitForCompletion(b2);
+				j.assertBuildStatus(Result.FAILURE, b2);
 				
 				RoundhouseAction action1 = b1.getAction(RoundhouseAction.class);
 				assertEquals(Style.THUMB_UP, action1.getStyle());
@@ -130,7 +111,5 @@ public class ChuckNorrisStepTest {
 				RoundhouseAction projectAction = p.getAction(RoundhouseAction.class);
 				assertEquals(Style.BAD_ASS, projectAction.getStyle());
 				assertNotNull(projectAction.getFact());
-			}
-		});
     }
 }
